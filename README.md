49e6eaa1bfdd5786ce8acf4004af6ad855fb8e7e
lmcevoy <lmcevoy@stevens.edu>
2020-08-01 17:16:42 -0400

    camera starts, no analysis or capture capability yet

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cameraXFramework
 Basic android app using CameraX
+
+Used Google's Codelab lesson: "Getting Started with CameraX" for education.
--- app/src/main/java/com/mcevoy/cameraxframework/MainActivity.kt
+++ app/src/main/java/com/mcevoy/cameraxframework/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.mcevoy.cameraxframework
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.camera.core.Camera
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 import java.io.File
+import java.lang.Exception
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
     private var preview: Preview? = null
@@ -44,14 +48,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            preview = Preview.Builder()
+                    .build()
+
+            val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                cameraProvider.unbindAll()
+
+                camera = cameraProvider.bindToLifecycle(
+                        this,
+                        cameraSelector,
+                        preview)
+
+                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
+            } catch (e: Exception) {
+                Timber.e("Use case binding failed $e")
+            }
+        }, ContextCompat.getMainExecutor(this))
 
     }
 
     private fun takePhoto() {
 
     }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 
-    private fun allPermissionsGranted() = false
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user ...",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
 
     fun getOutputDirectory(): File {
         val mediaDir = externalMediaDirs.firstOrNull()?.let {
